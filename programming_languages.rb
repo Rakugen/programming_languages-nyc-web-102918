669aaa3b2f893df17e036aa6f709d6374c3c6a53
Simon Lee <simonlee0306@gmail.com>
2018-10-24 21:26:28 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -9,7 +9,8 @@ def reformat_languages(languages)
 #      temp_type = value[0]
 #      new_hash[key] = {temp_type, :style => []}
       new_hash[key] = {:type => value[:type], :style => style_array}
-      new_hash[key][:style] << style
+#      new_hash[key][:style] << style
+      style_array << style
     end
   end
   new_hash
