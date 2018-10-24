6f91a54464c5a683c80dd76151eab2af422fd29f
Simon Lee <simonlee0306@gmail.com>
2018-10-24 21:13:00 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -7,6 +7,7 @@ def reformat_languages(languages)
 
     language.each do |key, value| #:ruby, :javascript, :python, etc
       new_hash[key] = {:type => value, :style => []}
+      new_hash[key][:type] = value
       new_hash[key][:style] << style
     end
   end
